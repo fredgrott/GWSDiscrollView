a4445096605f4c0615468f58f8168663e498cadd
Fred Grott <fred.grott@gmail.com>
2015-10-10 20:39:31 -0500

    added app demo code

---
+++ app/src/main/java/com/github/shareme/gwsdiscrollview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.shareme.gwsdiscrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
